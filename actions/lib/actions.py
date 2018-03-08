844750d74d7853e3bd3c2a9e983caa55e8cd30c0
John Anderson <lampwins@gmail.com>
2018-03-08 11:27:24 -0500

    pack update and move pandevice

---
+++ actions/lib/actions.py
@@ -0,0 +1,38 @@
+import pandevice
+
+from st2common.runners.base_action import Action
+
+
+class BaseAction(Action):
+    def __init__(self, config):
+        super(BaseAction, self).__init__(config)
+        if config is None:
+            raise ValueError("No connection configuration details found")
+        if "firewall" in config:
+            if config['firewall'] is None:
+                raise ValueError("'firewall' config defined but empty.")
+            else:
+                pass
+        else:
+            raise ValueError("No connection configuration details found")
+
+    def get_pandevice(self, firewall):
+        if firewall:
+            firewall_config = self.config['firewall'].get(firewall)
+        else:
+            firewall_config = self.config['firewall'].get('default')
+
+        # no need to duplicate config validation because pandevice is verbose enough
+        device = None
+        try:
+            device = pandevice.base.PanDevice.create_from_device(
+                hostname=firewall_config.get('hostname'),
+                api_username=firewall_config.get('api_username'),
+                api_password=firewall_config.get('api_password'),
+                api_key=firewall_config.get('api_key'),
+                port=firewall_config.get('port'),
+            )
+        except pandevice.errors.PanDeviceError as e:
+            raise Exception("Failed to connect to firewall {} with pandevice error {}".format(firewall_config, e))
+
+        return device
---
+++ actions/register_ip.py
@@ -0,0 +1,22 @@
+from lib.actions import BaseAction
+
+
+class RegisterIP(BaseAction):
+    """
+    Register IP/tag(s) to a firewall
+    """
+    def run(self, ip, tag, tag_list, firewall):
+
+        if not tag and not tag_list:
+            return False, "You must specify either tag or tag_list!"
+
+        if tag and tag_list:
+            return False, "You must only specify either tag OR tag_list but not both!"
+
+        register_tag = tag or tag_list
+
+        device = self.get_pandevice(firewall)
+
+        device.userid.register(ip, register_tag)
+
+        return True, "{} successfully registered to {}".format(register_tag, ip)
